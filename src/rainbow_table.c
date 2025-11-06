1bc48bde4378f533292aa581bc17cb8fbf888ddd
rens <rens@RRpro.local>
2025-11-07 00:41:39 +0100

    Final commit

--- src/rainbow_table.c
+++ src/rainbow_table.c
@@ -2,8 +2,12 @@
 #include <stdio.h> //FILE*, fopen, fprintf, perror
 #include <stdlib.h> //malloc, realloc, free
 #include <string.h> //strlen, strcmp, strchr, strcpy
-#include <errno.h> 
 #include <openssl/sha.h> //hashage SHA256
+
+typedef struct { //On crée une structure pour pouvoir afficher en plus du
+    char *hash; //hash
+    char *text; //le text en clair egalement lors du lookup
+} HashText; //et on nomme la structure HashText
 
 int generate(char *dic, char *out) { //2 paramètres pointeur vers le fichier de lecture et d'écriture
     FILE *open = fopen(dic, "r"); //Pointe vers l'objet type FILE, et ouvre en mode lecture
@@ -48,6 +52,12 @@ int generate(char *dic, char *out) { //2 paramètres pointeur vers le fichier de
     return 0;
 }
 
+int compare_hashes(const void *a, const void *b) { //On crée la fonction pratique de comparaison pour qsort et bsearch
+    const HashText *ptrA = (const HashText*)a; //param a c'est un pointeur vers struct HashText
+    const HashText *ptrB = (const HashText*)b; //idem pour b
+    return strcmp(ptrA->hash, ptrB->hash); //et on compare seulement les has entre structures
+}
+
 int lookup(char *table, char *hashes) { //2 paramètres pointeurs vers les fichiers à comparer
     FILE *open = fopen(table, "r"); //Ouvre le fichier en lecture
     if (open == NULL) { //Si le fichier manque 
@@ -58,7 +68,7 @@ int lookup(char *table, char *hashes) { //2 paramètres pointeurs vers les fichi
 
     size_t tableSize = 1024; //Taille aléatoire donnée à la table en octet
     size_t lineCount = 0; //Counter pour le nombre de ligne
-    char **hash1 = malloc(tableSize * sizeof(char*)); //Pointeurs vers pointeurs
+    HashText *hash1 = malloc(tableSize * sizeof(HashText)); //Pointeurs vers pointeurs
     if (hash1 == NULL) { //Erreur d'allocation memoire
         perror("malloc hash1"); //Imprime le message d'erreur
         fclose(open); //On ferme le fichier
@@ -82,7 +92,7 @@ int lookup(char *table, char *hashes) { //2 paramètres pointeurs vers les fichi
 
         if (lineCount == tableSize) { //Lorsqu'on a atteint les 1024 lignes
             tableSize *= 2; //On double la taille de Table
-            char **hash2 = realloc(hash1, tableSize * sizeof(char*));//Et on alloue dynamiquement la mémoire
+            HashText *hash2 = realloc(hash1, tableSize * sizeof(HashText));//Et on alloue dynamiquement la mémoire
             if (hash2 == NULL) {
                 perror("realloc");
                 free(linePtr);
@@ -92,30 +102,53 @@ int lookup(char *table, char *hashes) { //2 paramètres pointeurs vers les fichi
             hash1 = hash2;
         }
 
-        hash1[lineCount] = malloc (hashLen + 1); //Allocation mémoire pour copier le hash, +1 pour '\0'
-        if (hash1[lineCount] == NULL) { //Si erreur
-            perror("malloc hash1[count]");
+        char *text = tab + 1; //on défini un pointeur qui pointe vers le text clair apres le tab
+        size_t textLen = strlen(text); //et on prend son nombre de caracteres
+
+        hash1[lineCount].hash = malloc (hashLen + 1); //Allocation mémoire pour copier le hash, +1 pour '\0'
+        if (hash1[lineCount].hash == NULL) { //Si erreur
+            perror("malloc hash");
             for (size_t k = 0; k < lineCount; k++) {
-                free(hash1[k]); //On libère les hashes copiés
+                free(hash1[k].hash); //On libère les hashes copiés
+                free(hash1[k].text); //idem pour les texts clairs copiés
             }
+            free(hash1); //On libère le pointeur vers struct
             free(linePtr); //On libère le pointeur
             fclose(open); //On ferme le fichier
             return 1;
         }
-        strcpy(hash1[lineCount], linePtr); //Sinon on copy le hash à l'adresse allouée
+        strcpy(hash1[lineCount].hash, linePtr); //Sinon on copy le hash à l'adresse allouée
+
+        hash1[lineCount].text = malloc(textLen + 1); //on alloue la taille necessaire pour le text clair
+        if (hash1[lineCount].text == NULL) { //si erreur d'allocation
+            perror("malloc text"); //msg d'erreur et
+            free(hash1[lineCount].hash); //on libere le hash qu'on vient de stocker
+            for (size_t k = 0; k < lineCount; k++) { //et pour chauqe ligne en mémoire
+                free(hash1[k].hash); //on libère le pointeur vers le hash
+                free(hash1[k].text); //idem vers le text clair
+            }
+            free(hash1); //on libere ensuite le pointeur vers struct
+            free(linePtr); //le pointeur buffer
+            fclose(open); //et on ferme la table
+            return 1;
+        }
+        strcpy(hash1[lineCount].text, text); //on copie le text en clair dans l'espace memoire alloué
+
         lineCount++; //On le compte
     }
     free(linePtr); //On libère le buffeur
     fclose(open); //On ferme la table
 
 
+    qsort(hash1, lineCount, sizeof(HashText), compare_hashes); //on trie le tableau de hash en mémoire pour bsearch
 
     FILE *read = fopen(hashes, "r"); //On ouvre en lecture le ficher de hashes à comparer
     if (read == NULL) { //Si erreur
         fprintf(stderr, "fopen %s: ", hashes); //On imprime le chemin du fichier
         perror(NULL); //Avec l'erreur système
         for (size_t i = 0; i < lineCount; i++) {
-            free(hash1[i]); //On libère les hash
+            free(hash1[i].hash); //On libère les hash
+            free(hash1[i].text); //idem pour le text en clair
         }
         free(hash1); //On libère le buffer
         return 1;
@@ -135,20 +168,24 @@ int lookup(char *table, char *hashes) { //2 paramètres pointeurs vers les fichi
             continue;
         }
 
-        for (size_t i = 0; i < lineCount; i++) { //Cherche parmi toutes les lignes de la tables
-            if (strcmp(buffer, hash1[i]) == 0) { //Une correspondance exacte 
-                printf("%s\n", buffer); //L'affiche en sortie standard
-                found++; //Augmente le counter
-                break; //Stop la boucle
-            }
+        HashText search_text; //on créer une struct temp
+        search_text.hash = buffer; //et défini le buffer pour le hash qu'on cherche
+        search_text.text = NULL; //pas besoin de .text, bsearch compare que les .hash
+
+        HashText *result = bsearch(&search_text, hash1, lineCount, sizeof(HashText), compare_hashes); //on implement la fonction de recherche binaire
+        if (result != NULL) { //si la bsearch trouve un match dans la table, et est donc non-NULL
+            printf("%s\t%s", result->hash, result->text); //on l'affiche en sortie standard
+            found++; //et on augmente le counter
         }
+
         //printf("\n"); //À dé-commenter pour débugger
     }
     printf("Hashes found: %d\n", found); //Affiche le nombre de hits
     fclose(read); //Ferme le fichier
     
     for(size_t i = 0; i < lineCount; i++) {
-        free(hash1[i]); //Libère l'espace alloué pour les hash de la table
+        free(hash1[i].hash); //Libère l'espace alloué pour les hash de la table
+        free(hash1[i].text); //et celui alloué pour le text clair
     }
     free(hash1); //Puis le pointeur
     return 0;
